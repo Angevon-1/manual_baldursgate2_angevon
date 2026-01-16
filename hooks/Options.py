0e065b4fc95c3a7480c058698857108da2202242
Angevon-1 <angevon@gmail.com>
2026-01-16 13:07:15 -0500

    add line breaks to work with the options gui in the AP launcher

--- hooks/Options.py
+++ hooks/Options.py
@@ -28,12 +28,14 @@ from typing import Type, Any
 
 class include_irenicus_dungeon(Toggle):
     """Adds key items and locations related to Chapter 1 (Irenicus's Dungeon).
-    When false, it is recommended that you use the Skip Chateau Irenicus mod to get out of there easier after starting a new game."""
+    When false, it is recommended that you use the Skip Chateau Irenicus mod
+    to get out of there easier after starting a new game."""
     display_name = "Include Irenicus's Dungeon"
     default = True
 
 class final_chapter(Choice):
-    """Select which chapter to finish the game in. When you choose a later chapter, the previous chapters are automatically enabled. 
+    """Select which chapter to finish the game in. 
+    When you choose a later chapter, the previous chapters are automatically enabled. 
     Later chapter = longer game, more items, and more locations.
     Some goal options override this choice."""
     option_chapter3 = 0
@@ -45,26 +47,36 @@ class final_chapter(Choice):
     default = 0
 
 class include_city_of_caverns(Toggle):
-    """When this option is true, key items and locations related to the optional Chapter 4 area, City of Caverns/Sahaguin City will be included in the multiworld.
-    When this is on, you'll need to take Saemon's offer to sail away from Spellhold at the end of that questline. If you're not using this option, you can take the portal under Spellhold directly to Chapter 5."""
+    """When this option is true, key items and locations related to the optional Chapter 4 area, 
+    City of Caverns/Sahaguin City will be included in the multiworld.
+    When this is on, you'll need to take Saemon's offer to sail away from Spellhold at the end of that questline. 
+    If you're not using this option, you can take the portal under Spellhold directly to Chapter 5."""
     display_name = "Include City of Caverns"
     default = False
 
 class gaelan_or_bodhi(Choice):
-    """Choose whether you will side with Gaelan Bayle and the Shadow Thieves, or with Bodhi and the vampires, in Chapter 3.
-    This affects the names of item and location checks, so be sure to match this with your choice in the game."""
+    """Choose whether you will side with Gaelan Bayle and the Shadow Thieves, 
+    or with Bodhi and the vampires, in Chapter 3.
+    This affects the names of item and location checks, 
+    so be sure to match this with your choice in the game."""
     display_name = "Side in Chapter 3"
     option_shadow_thieves = 0    
     option_bodhi = 1
     default = 0
 
 class misc_items(Choice):
-    """Choose how you'd like miscellaneous items in the item pool. Miscellaneous items are things like the spider figurine and the harp of discord.
-    No Misc. Items- No miscellaneous items are in the pool. This works best with the Gibberlings 3 Item Randomizer mod. 
-    Base Game Misc. Items Only - Miscellaneous items from the base game are added to the pool. Does not include Collector's Edition or Enhanced Edition items. 
-    Base Game Misc. Items and Collector's Edition - Miscellaneous items from the base game are added to the pool, including Collector's Edition items. 
-    Base Game Misc. Items and Enhanced Edition - Miscellaneous items from the base game are added to the pool, including Enhanced Edition items.
-    Base Game Misc. Items, Collector's Edition, and Enhanced Edition - Miscellaneous items from the base game are added to the pool, including Collector's Edition and Enhanced Edition items.
+    """Choose how you'd like miscellaneous items in the item pool. 
+    Miscellaneous items are things like the spider figurine and the harp of discord.
+    No Misc. Items- No miscellaneous items are in the pool. 
+    This works best with the Gibberlings 3 Item Randomizer mod. 
+    Base Game Misc. Items Only - Miscellaneous items from the base game are added to the pool. 
+    Does not include Collector's Edition or Enhanced Edition items. 
+    Base Game Misc. Items and Collector's Edition - Miscellaneous items from the base game are added to the pool, 
+    including Collector's Edition items. 
+    Base Game Misc. Items and Enhanced Edition - Miscellaneous items from the base game are added to the pool, 
+    including Enhanced Edition items.
+    Base Game Misc. Items, Collector's Edition, and Enhanced Edition - Miscellaneous items from the base game are added to the pool, 
+    including Collector's Edition and Enhanced Edition items.
     """
     option_no_misc_items = 0
     option_base_game_only = 1
@@ -75,12 +87,17 @@ class misc_items(Choice):
     display_name = "Misc. Items in pool"
 
 class equipment(Choice):
-    """Choose how you'd like equipment in the item pool. Please note the more equipment you add, the more chapters you should include too, else items will be lost during generation.
+    """Choose how you'd like equipment in the item pool. Please note the more equipment you add, 
+    the more chapters you should include too, else items will be lost during generation.
     No Equipment - No equipment is in the pool. This works best with the Gibberlings 3 Item Randomizer mod. 
-    Base Game Magical Equipment Only - Each individual magical equipment item from the base game is added to the pool. Does not include Collector's Edition or Enhanced Edition items. 
-    Base Game Magical Equipment and Collector's Edition - Each individual magical equipment item from the base game is added to the pool, including Collector's Edition items. These items might be considered overpowered.
-    Base Game Magical Equipment and Enhanced Edition - Each individual magical equipment item from the base game is added to the pool, including Enhanced Edition items.
-    Base Game Magical Equipment, Collector's Edition, and Enhanced Edition - Each individual magical equipment item from the base game is added to the pool, including Collector's Edition and Enhanced Edition items.
+    Base Game Magical Equipment Only - Each individual magical equipment item from the base game is added to the pool. 
+    Does not include Collector's Edition or Enhanced Edition items. 
+    Base Game Magical Equipment and Collector's Edition - Each individual magical equipment item from the base game is added to the pool, 
+    including Collector's Edition items. These items might be considered overpowered.
+    Base Game Magical Equipment and Enhanced Edition - Each individual magical equipment item from the base game is added to the pool, 
+    including Enhanced Edition items.
+    Base Game Magical Equipment, Collector's Edition, and Enhanced Edition - Each individual magical equipment item from 
+    the base game is added to the pool, including Collector's Edition and Enhanced Edition items.
     """
     option_no_equipment = 0
     option_base_game_only = 1
@@ -91,24 +108,30 @@ class equipment(Choice):
     display_name = "Equipment in pool"
 
 class progressive_equipment(Toggle):
-    """Use this option to make equipment progressive. You can't use +x weapons until you've received the same number of progressives for that weapon type. For example, the Namarra short sword is a +2 short sword. You'll need two “Progressive Short Sword” items in order to be able to use it. Works well with the Gibberlings 3 Item Randomizer mod.
-    This CAN be combined with the equipment option choice above, however items will likely be lost during generation.
-    Be aware that currently the logic does not account for whether you've received enough progressives to defeat enemies that require certain +x's (golems, jellies, etc.)."""
+    """Use this option to make equipment progressive. You can't use +x weapons until you've received 
+    the same number of progressives for that weapon type. 
+    For example, the Namarra short sword is a +2 short sword. 
+    You'll need two “Progressive Short Sword” items in order to be able to use it. 
+    This option works well with the Gibberlings 3 Item Randomizer mod.
+    This CAN be combined with the equipment option above, however items will likely be lost during generation."""
     default = 1
     display_name = "Progressive Equipment"
 
 class include_watchers_keep(Toggle):
-    """When this is true, it adds equipment from Watcher's Keep to the multiworld. When false, they aren't added.
-    Note: watcher's keep location checks are NOT affected, they are not implemented at all at this time, this is purely about the item rewards."""
+    """When this is true, equipment from Watcher's Keep is added to the multiworld. When false, they aren't added.
+    Note: Watcher's Keep location checks are NOT affected, they are not implemented at all at this time, 
+    this is purely about the item rewards."""
     display_name = "Add Watcher's Keep Items"
     default = False
 
 class companions(Choice):
     """Choose how you'd like the companion NPCs (items) & their quests (locations) randomized. 
     None - Companions and their quests are not included in the multiworld. 
-    Companions Only - Each individual non-EE companion is an item. To see what companions are available to you, open the Manual Client and connect to your room. Then open the Manual tab. Then expand the 'Companions' section.
+    Companions Only - Each individual non-EE companion is an item. To see what companions are available to you, 
+    open the Manual Client and connect to your room. Then open the Manual tab. Then expand the 'Companions' section.
     Companions Only Including Enhanced Edition - as above, but includes the Enhanced Edition companions.
-    Quests Only - Adds location checks for the non-EE companion quests. These quests can take a long time to activate in the game; speedrunners should not use any option that includes quests.  
+    Quests Only - Adds location checks for the non-EE companion quests. These quests can take a long time to activate in the game; 
+    speedrunners should not use any option that includes quests.  
     Quests Only Including Enhanced Edition - as above, but includes the Enhanced Editions companions and their quests and areas.
     Companions and Quests - Adds non-EE companions and their quests. 
     Companions and Quests Including Enhanced Edition - Adds all companions and quests and areas, including Enhanced Edition.
@@ -124,7 +147,9 @@ class companions(Choice):
     display_name = "Companion Option"
 
 class starting_companion_amount(Range):
-    """When companions are randomized, use this option to set how many companions you'll already have at the start of the game. They will be added at random to your starting inventory. You'll still have to go and recruit them.
+    """When companions are randomized, use this option to set how many companions you'll 
+    already have at the start of the game. They will be added at random to your starting inventory. 
+    You'll still have to go and recruit them.
     Leave this at 0 if you're not randomizing the companions.""" 
     display_name = "Starting Companion Amount"
     range_start = 0
@@ -135,7 +160,9 @@ class loot_checks(Choice):
     """Choose how you want loot checks to work.
     None - No loot checks.
     Rooms - Adds general "loot x room" checks. Lockpicking ability is not necessarily required.
-    Containers - Adds every INDIVIDUAL lootable crate/barrel/chest as location checks. To compensate for check count, consumable potions and scrolls will be added to the item pool to add filler. Lockpicking is required and NOT accounted for. Progression may be locked behind a locked chest and it's up to you to get past it."""
+    Containers - Adds every INDIVIDUAL lootable crate/barrel/chest as location checks. To compensate for check count, 
+    consumable scrolls will be added to the item pool to add filler. Lockpicking is required and NOT accounted for. 
+    Progression may be locked behind a locked chest and it's up to you to get past it."""
     display_name = "Loot checks type"
     option_none = 0
     option_rooms = 1
@@ -144,7 +171,8 @@ class loot_checks(Choice):
     default = 0
 
 class forging_checks(Toggle):
-    """Set this to true if you'd like to earn a check for forging items like Crom Faeyr, Flail of the Ages, etc. Adds 11 checks and 20 items."""
+    """Set this to true if you'd like to earn a check for forging items like Crom Faeyr, Flail of the Ages, etc. 
+    Adds 11 checks and 20 items."""
     display_name = "Include Forging Checks"
     default = False
     
